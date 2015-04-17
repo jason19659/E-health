98343ef4eda519811629119abd9d6cd5d1036e9e
jason <jason19659@gmail.com>
2015-04-17 13:20:30 +0800

    3

---
+++ src/com/jason19659/ehealth/service/BaseService.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.jason19659.ehealth.service;
+
+
+/**
+ * @author <a href="mailto:jason19659@163.com">jason19659</a>
+ *
+ * com.jason19659.ehealth.service
+ *
+ * 2015年4月17日
+ */
+public interface BaseService<T> {
+	 int deleteByPrimaryKey(String id);
+
+	    int insert(T record);
+
+	    int insertSelective(T record);
+
+	    T selectByPrimaryKey(String id);
+
+	    int updateByPrimaryKeySelective(T record);
+
+	    int updateByPrimaryKey(T record);
+}
