b5b712997c7b52d7c07143c68b547c3545ecf24f
jason <jason19659@gmail.com>
2015-05-21 08:30:42 +0800

    update

---
+++ src/com/jason19659/ehealth/interceptor/ComptenceInterceptor.java
@@ -0,0 +1,113 @@
+package com.jason19659.ehealth.interceptor;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jason19659.ehealth.model.User;
+
+public class ComptenceInterceptor implements HandlerInterceptor,Filter  {
+
+	
+	public ComptenceInterceptor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setMappingURL(String mappingURL) {  
+       } 
+
+	/**
+	 * 在业务处理器处理请求之前被调用
+	 * 如果返回false
+	 *     从当前的拦截器往回执行所有拦截器的afterCompletion(),再退出拦截器链
+	 * 
+	 * 如果返回true
+	 *    执行下一个拦截器,直到所有的拦截器都执行完毕
+	 *    再执行被拦截的Controller
+	 *    然后进入拦截器链,
+	 *    从最后一个拦截器往回执行所有的postHandle()
+	 *    接着再从最后一个拦截器往回执行所有的afterCompletion()
+	 * @throws IOException 
+	 * @throws ServletException 
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String path = request.getContextPath();
+		String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+		try {
+			String comptence = ((User)session.getAttribute("user")).getComptence();
+			if (comptence.contains("admin") || comptence.contains("root")) {
+				return true;
+			}
+			request.getRequestDispatcher(basePath).forward(request, response);
+			return false;
+		} catch (Exception e) {
+			request.getRequestDispatcher(basePath).forward(request, response);
+			return false;
+		}
+		
+	}
+
+	//在业务处理器处理请求执行完成后,生成视图之前执行的动作 
+	@Override
+	public void postHandle(HttpServletRequest request,
+			HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+	}
+
+	/**
+	 * 在DispatcherServlet完全处理完请求后被调用 
+	 * 
+	 *   当有拦截器抛出异常时,会从当前拦截器往回执行所有的拦截器的afterCompletion()
+	 */
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+			HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+	}
+
+	/* 
+	 * @see javax.servlet.Filter#destroy()
+	 */
+	@Override
+	public void destroy() {
+		
+	}
+
+	/* 
+	 * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest, javax.servlet.ServletResponse, javax.servlet.FilterChain)
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		try {
+			preHandle((HttpServletRequest)request,(HttpServletResponse)response, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		chain.doFilter(request, response);
+		
+	}
+
+	/* 
+	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+	 */
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+
+}
